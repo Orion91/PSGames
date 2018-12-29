25255be4febc47ba62edb6501dde8866f0fe517b
Orion91 <flinch217@gmail.com>
2018-12-29 13:39:55 +0100

    Added client side validation to registration form

--- PSGames-SPA/src/app/register/register.component.ts
+++ PSGames-SPA/src/app/register/register.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit, Output, EventEmitter } from '@angular/core';
 import { AuthService } from '../_services/auth.service';
+import { FormGroup, Validators, FormBuilder } from '@angular/forms';
 
 @Component({
   selector: 'app-register',
@@ -9,10 +10,24 @@ import { AuthService } from '../_services/auth.service';
 export class RegisterComponent implements OnInit {
   @Output() cancelRegister = new EventEmitter();
   model: any = {};
+  registerForm: FormGroup;
 
-  constructor(private authService: AuthService) { }
+  constructor(private authService: AuthService, private fb: FormBuilder) { }
 
   ngOnInit() {
+    this.createRegisterForm();
+  }
+
+  createRegisterForm() {
+    this.registerForm = this.fb.group({
+      username: ['', Validators.required],
+      password: ['', [Validators.required, Validators.minLength(6)]],
+      confirmPassword: ['', Validators.required]
+    }, {validator: this.passwordMatchValidator});
+  }
+
+  passwordMatchValidator(g: FormGroup) {
+    return g.get('password').value === g.get('confirmPassword').value ? false : {'mismatch': true};
   }
 
   register() {
--- PSGames.API/DTOs/UserForRegisterDto.cs
+++ PSGames.API/DTOs/UserForRegisterDto.cs
@@ -7,6 +7,7 @@ namespace PSGames.API.DTOs
     {
         [Required]
         public string Username { get; set; }
+        
         [Required]
         [StringLength(16, MinimumLength = 8, ErrorMessage = "You must specify password between 8 and 16 characters")]
         public string Password { get; set; }
